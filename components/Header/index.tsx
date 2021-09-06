3c940969b11f694fcf16ff67d798f331dc0a5459
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-09-05 21:19:13 -0300

    Init app | Add header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./index.module.css";
+
+const Header = () => (
+    <div className={styles.container}>
+        <header>
+            <section className={styles.titleContainer}>
+                <div>
+                    <Image src="/assets/logo.svg" alt="Logo" width="100" height="100"/>
+                    <h1>MARIMAR NASCIMENTO</h1>
+                    <small>PSICÓLOGA | CRP 05/6100</small>
+                </div>
+                <div>
+                    Telefone/WhatsApp
+                    (21) 97400-9839
+                </div>
+            </section>
+            <section className={styles.navContainer}>
+                <nav>
+                    <Link href="/">Início</Link>
+                    <Link href="/sobre">Sobre</Link>
+                    <Link href="/atuacao">Atuação</Link>
+                    <Link href="/contato">Contato</Link>
+                    <Link href="/blog">Blog</Link>
+                </nav>
+            </section>
+        </header>
+        <section>
+            <h2>Psicoterapia individual online para adultos e idosos.</h2>
+            <button>ENTRE EM CONTATO</button>
+        </section>
+    </div>
+);
+
+export default Header;
