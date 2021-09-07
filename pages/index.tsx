04571385847f073ab8a6ac5d3f8b2b1dd11b0f9b
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-09-06 21:31:45 -0300

    Add Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,20 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
+      <main>
+        <div className={styles.quote}>
+          <p>
+            "Se pudermos reorientar nossos pensamentos e emoções e reorganizar nosso comportamento, 
+            então poderemos não só aprender a lidar com o sofrimento mais facilmente, 
+            mas, sobretudo e em primeiro lugar, evitar que muito dele surja."
+          </p>
+          <p>
+            Dalai Lama (1999)
+          </p>
+        </div>
+      </main>
     </div>
   )
 }
