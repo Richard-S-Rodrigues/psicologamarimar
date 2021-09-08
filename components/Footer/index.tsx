dab6013fa8de6fc1358b7ed143fa042d3b2bd285
root <root@DESKTOP-VH2R3L3.localdomain>
2021-09-07 22:02:33 -0300

    Add Footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+import styles from "./index.module.css";
+
+const Footer = () => (
+  <div className={styles.container}>
+    <div className={styles.mainContainer}>
+      <section className={styles.contactsContainer}>
+        <div>
+          <Image
+            src="/assets/logo-black.svg"
+            alt="Logo"
+            width="50"
+            height="50"
+          />
+        </div>
+        <div>
+          <p>
+            <Image
+              src="/assets/whatsapp-icon.svg"
+              alt="Ícone do WhatSapp"
+              width="1"
+              height="1"
+              layout="fixed"
+            />
+            (21) 97400-9839
+          </p>
+          <p>
+            <Image
+              src="/assets/mail-icon.svg"
+              alt="Ícone de E-mail"
+              width="1"
+              height="1"
+              layout="fixed"
+            />
+            marimarnascimento@gmail.com
+          </p>
+        </div>
+      </section>
+      <section className={styles.aboutContainer}>
+        <div>
+          <Image
+            src="/assets/photo.jpg"
+            alt=""
+            width="100"
+            height="100"
+            layout="fixed"
+          />
+          <div>
+            <h3>Marimar Nascimento</h3>
+            <small>Psicóloga</small>
+          </div>
+        </div>
+        <div>
+          <p>
+            Graduada em Psicologia pela UFRJ, atualmente pós graduanda em
+            Psicogerontologia e com formação em Terapia Cognitivo Comportamental
+            (TCC) em andamento...
+          </p>
+          <button>SAIBA MAIS</button>
+        </div>
+      </section>
+    </div>
+    <div>Copyright © 2021. Todos os direitos reservados.</div>
+  </div>
+);
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
-import type { NextPage } from 'next'
-import Header from '../components/Header'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -9,17 +12,17 @@ const Home: NextPage = () => {
       <main>
         <div className={styles.quote}>
           <p>
-            "Se pudermos reorientar nossos pensamentos e emoções e reorganizar nosso comportamento, 
-            então poderemos não só aprender a lidar com o sofrimento mais facilmente, 
-            mas, sobretudo e em primeiro lugar, evitar que muito dele surja."
+            "Se pudermos reorientar nossos pensamentos e emoções e reorganizar
+            nosso comportamento, então poderemos não só aprender a lidar com o
+            sofrimento mais facilmente, mas, sobretudo e em primeiro lugar,
+            evitar que muito dele surja."
           </p>
-          <p>
-            Dalai Lama (1999)
-          </p>
+          <p>Dalai Lama (1999)</p>
         </div>
       </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
