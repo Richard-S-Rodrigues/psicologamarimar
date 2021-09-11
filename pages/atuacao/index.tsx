3b9f8d1061664cfbd40a467fd6471cae9bf5f65c
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-09-10 21:10:34 -0300

    Add 'Atuacao' page

---
+++ pages/atuacao/index.tsx
@@ -0,0 +1,42 @@
+import styles from "./index.module.css";
+
+const Atuacao = () => (
+  <div className={styles.container}>
+    <main>
+      <h1>Terapia Cognitivo-Comportamental (TCC)</h1>
+      <div>
+        <p>
+          É comum pensar que a psicologia é algo "único" e será aplicada na
+          terapia da mesma forma por todo e qualquer psicólogo. No entanto, ela
+          é um campo muito diverso e amplo, compreendendo diversas linhas de
+          atuação. Ou seja, a psicoterapia possui diferentes abordagens, e cada
+          psicólogo utiliza-se de pelo menos uma delas em sua atuação.
+        </p>
+      </div>
+      <div>
+        <p>
+          Minha abordagem dentro da Psicologia é a Terapia
+          Cognitivo-Comportamental (TCC). De forma simplificada, a TCC é baseada
+          na ideia de que as nossas cognições (pensamentos) têm grande
+          influência sobre nossas emoções e comportamentos. Isso significa que,
+          quando vivenciamos alguma situação, não é a situação em si que vai
+          determinar nossas ações e emoções perante ela, mas sim a forma como
+          interpretamos essa situação.{" "}
+        </p>
+      </div>
+      <div>
+        <p>
+          As teorias e métodos da TCC foram desenvolvidas inicialmente pelo
+          médico e psicanalista Aaron T. Beck por volta das décadas de 1960 e
+          1970 e influenciaram a evolução de diversas técnicas mais específicas
+          utilizadas atualmente. Inicialmente focada nos quadros de depressão e
+          ansiedade, hoje a TCC também é utilizada para tratar casos de
+          transtornos alimentares, bipolaridade, transtornos de personalidade,
+          esquizofrenia, dor crônica, abuso de substâncias, entre outros.
+        </p>
+      </div>
+    </main>
+  </div>
+);
+
+export default Atuacao;
