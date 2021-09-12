d9c3d3957d83a809ab411c38ba51482efd019152
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-09-11 23:26:21 -0300

    Add Contact form

---
+++ pages/api/contact.ts
@@ -0,0 +1,59 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import { Request, Response } from "express";
+
+interface BodyData {
+  name: string;
+  email: string;
+  tel: string;
+  message: string;
+}
+
+interface MailOptionsData {
+  from: string;
+  to: string;
+  subject: string;
+  text: string;
+}
+
+export default function (req: Request, res: Response) {
+  const { name, email, tel, message }: BodyData = req.body;
+
+  const nodemailer = require("nodemailer");
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "marimarnascimento@gmail.com",
+        pass: process.env.PASS,
+      },
+    });
+
+    const mailOptions: MailOptionsData = {
+      from: email,
+      to: "richardsouzarodrigues555@gmail.com",
+      subject: "psicoterapia",
+      text: `
+      \nE-MAIL: ${email},
+      \nNOME: ${name},
+      \nTELEFONE: ${tel}
+      \n---------------------------------------------------------------- 
+      \n${message}
+      `,
+    };
+
+    transporter.sendMail(mailOptions, function (err: any, info: any) {
+      if (err) {
+        throw new Error(err);
+      } else {
+        console.log(`Email sent: ${info.response}`);
+      }
+    });
+
+    return res.status(201).send();
+  } catch (error) {
+    console.log("Error: ", error);
+    return res.status(400).send();
+  }
+}
--- pages/contato/index.tsx
+++ pages/contato/index.tsx
@@ -1,39 +1,125 @@
+import { useState } from "react";
+
 import styles from "./index.module.css";
 
-const Contact = () => (
-  <div className={styles.container}>
-    <main>
-      <div className={styles.formContainer}>
-        <h2>Você pode tirar suas dúvidas pelo formulário</h2>
-        <form>
-          <div>
-            <label htmlFor="name">NOME</label>
-            <input type="text" name="name" id="name" />
-          </div>
-          <div>
-            <label htmlFor="email">E-MAIL</label>
-            <input type="text" name="email" id="email" />
-          </div>
-          <div>
-            <label htmlFor="tel">CELULAR</label>
-            <input type="text" name="tel" id="tel" />
-          </div>
-          <div>
-            <label htmlFor="message">MENSAGEM</label>
-            <textarea name="message" id="message" />
-          </div>
-          <button>ENVIAR</button>
-        </form>
-      </div>
-      <div className={styles.contactContainer}>
-        <h2>Ou mandar mensagem diretamente por e-mail ou WhatsApp:</h2>
-        <ul>
-          <li>marimarnascimento@gmail.com</li>
-          <li>(21) 97400-9839</li>
-        </ul>
-      </div>
-    </main>
-  </div>
-);
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [isSent, setIsSent] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const onSubmitHandler = (event: any) => {
+    event.preventDefault();
+
+    if (!name || !email || !tel || !message) return;
+
+    const data = {
+      name,
+      email,
+      tel,
+      message,
+    };
+
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          console.log("Email sent!");
+
+          setIsSent(true);
+
+          setName("");
+          setEmail("");
+          setTel("");
+          setMessage("");
+        }
+      })
+      .catch((error) => {
+        setIsError(true);
+      });
+  };
+
+  return (
+    <div className={styles.container}>
+      {isSent && (
+        <div className={`${styles.success} ${styles.hide}`}>
+          Dúvida enviada com sucesso!
+        </div>
+      )}
+      {isError && (
+        <div className={`${styles.fail} ${styles.hide}`}>
+          Algo deu errado. Tente novamente!
+        </div>
+      )}
+      <main>
+        <div className={styles.formContainer}>
+          <h2>Você pode tirar suas dúvidas pelo formulário</h2>
+          <form onSubmit={onSubmitHandler}>
+            <div>
+              <label htmlFor="name">NOME</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="email">E-MAIL</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="tel">CELULAR</label>
+              <input
+                type="text"
+                name="tel"
+                id="tel"
+                value={tel}
+                onChange={(event) => setTel(event.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="message">MENSAGEM</label>
+              <textarea
+                name="message"
+                id="message"
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">ENVIAR</button>
+          </form>
+        </div>
+        <div className={styles.contactContainer}>
+          <h2>Ou mandar mensagem diretamente por e-mail ou WhatsApp:</h2>
+          <ul>
+            <li>marimarnascimento@gmail.com</li>
+            <li>(21) 97400-9839</li>
+          </ul>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default Contact;
