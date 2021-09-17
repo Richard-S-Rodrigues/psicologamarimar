cce13999883ae99feab7d95bdf2de06f5b3b03ef
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-09-17 20:23:21 -0300

    Add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,35 +8,50 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <main>
         <section className={styles.imgContainer}>
-          <div>
+          <div className={styles.image}>
             <Image
-              src="/assets/online-therapy.svg"
+              src="/assets/online-therapy.png"
               alt="Terapia online"
-              width="600"
-              height="600"
+              width="100"
+              height="100"
+              layout="responsive"
             />
+            <a
+              href="https://br.freepik.com/vetores/internet"
+              style={{
+                display: "flex",
+                fontSize: "12px",
+                justifyContent: "center",
+                marginTop: 0,
+                marginBottom: "2em",
+              }}
+              rel="noreferrer"
+              target="_blank"
+            >
+              Imagem criada por vectorjuice - br.freepik.com
+            </a>
           </div>
-          <div>
-            <h1>
+          <div className={styles.text}>
+            <h2>
               Acompanhamento psicoterapêutico online, para cuidar da sua saúde
               mental no conforto da sua casa.
-            </h1>
+            </h2>
           </div>
         </section>
         <section className={styles.postsContainer}>
           <h2>POSTAGENS RECENTES</h2>
           <p>Nenhuma postagem criada ainda</p>
         </section>
-        <section className={styles.quote}>
-          <p>
-            &quot;Se pudermos reorientar nossos pensamentos e emoções e
-            reorganizar nosso comportamento, então poderemos não só aprender a
-            lidar com o sofrimento mais facilmente, mas, sobretudo e em primeiro
-            lugar, evitar que muito dele surja.&quot;
-          </p>
-          <p>Dalai Lama (1999)</p>
-        </section>
       </main>
+      <div className={styles.quote}>
+        <p>
+          &quot;Se pudermos reorientar nossos pensamentos e emoções e
+          reorganizar nosso comportamento, então poderemos não só aprender a
+          lidar com o sofrimento mais facilmente, mas, sobretudo e em primeiro
+          lugar, evitar que muito dele surja.&quot;
+        </p>
+        <p>Dalai Lama (1999)</p>
+      </div>
     </div>
   );
 };
