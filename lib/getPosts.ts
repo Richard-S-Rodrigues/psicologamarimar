186255dee37f65bfdf008247474c6c03096695d2
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-10-08 17:32:10 -0300

    Add production environment variable

--- lib/getPosts.ts
+++ lib/getPosts.ts
@@ -3,7 +3,10 @@ import { createClient } from "contentful";
 const getPosts = async (postsLimit?: number) => {
   const client = createClient({
     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+    accessToken:
+      process.env.NODE_ENV == "production"
+        ? process.env.CONTENTFUL_ACCESS_TOKEN
+        : process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
   });
 
   const data = await client.getEntries({
