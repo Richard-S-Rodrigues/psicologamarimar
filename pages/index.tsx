3d2c202b9558a38c1ac933c435fa4d0ad0638e66
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-10-11 16:22:59 -0300

    Compress image size

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = ({ posts }: any) => {
         <section className={styles.imgContainer}>
           <div className={styles.image}>
             <Image
-              src="/assets/online-therapy.png"
+              src="/assets/online-therapy.jpg"
               alt="Terapia online"
               width="100"
               height="100"
