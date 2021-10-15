02d36077f43d0766022deadb308fcf8ebfb7d938
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-10-15 16:35:26 -0300

    Add google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import * as gtag from "../lib/gtag";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 import "../styles/globals.css";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,37 @@
 // eslint-disable-next-line
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+import { GA_TRACKING_ID } from "../lib/gtag";
+
+const isProduction = process.env.NODE_ENV === "production";
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
         <Head>
+          {/* enable analytics script only for production */}
+          {isProduction && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+              />
+            </>
+          )}
           <meta
             name="description"
             content="Psicologia clínica - atendimento online individual para adultos e idosos com abordagem na Terapia Cognitivo-Comportamental (TCC)"
